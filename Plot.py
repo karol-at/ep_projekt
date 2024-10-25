5e51480cec9875d1777b025570e8a9b57f5b958e
Karol Atys <karol.atys@gmail.com>
2024-10-25 16:15:41 +0200

    style: style graph and increase resolution

--- Plot.py
+++ Plot.py
@@ -6,13 +6,19 @@ from DataClasses import DataPoint
 
 def createplot(data: DataPoint) -> plt.Figure:
     fig, ax = plt.subplots()
-    fig.set_size_inches(6,4)
+    ax.xaxis.label.set_fontsize(18)
+    ax.yaxis.label.set_fontsize(18)
+    ax.yaxis.set_tick_params(labelsize = 14)
+    ax.xaxis.set_tick_params(labelsize = 14)
+    plt.rcParams.update({'font.size': 24})
+    fig.set_size_inches(18,10)
     ax.grid()
     ax.plot(
     [i.period for i in data],
     [i.value for i in data]
     )
     tickLabels = [roman.toRoman(i) for i in range(1, 13)]
+    fig.set_linewidth(18)
     ax.set(
         yticks = np.arange(
             floor(min(data, key = lambda x: x.value).value),
